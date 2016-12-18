f4bea45d220dffb1bdfaa78c1c55e0837c48696f
zhuyijing <zhuyijing168@163.com>
2016-12-18 16:02:07 +0800

    server select

--- go.py
+++ go.py
@@ -3,18 +3,22 @@
 
 from iss import get_account_from_iss
 import os
+import sys
 
-def run_sslocal():
+def run_sslocal(idx=0):
     accounts = get_account_from_iss()
 
-    for acc in accounts:
-        cmd = 'sslocal -s %s -p %s -b 0.0.0.0 -l %s -k %s -m %s -d restart' % (acc['server'],
-            acc['port'], '1080', acc['passwd'], acc['encrypt'])
-        print cmd
-        os.system(cmd)
-        break
+    acc = accounts[idx]
+    cmd = 'sslocal -s %s -p %s -b 0.0.0.0 -l %s -k %s -m %s -d restart' % (acc['server'],
+        acc['port'], '1080', acc['passwd'], acc['encrypt'])
+    print cmd
+    os.system(cmd)
 
 if __name__ == '__main__':
     print 'sslocal background monitor is running now...'
 
-    run_sslocal()
+    idx = 0
+    if len(sys.argv) > 1:
+        idx = int(sys.argv[1])
+        print idx
+    run_sslocal(idx)
